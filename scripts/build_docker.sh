7e6b9ce08ec3fd73b9052b2b5da8be6c5041674e
Osbert Ngok <osbertngok@gmail.com>
2018-04-01 16:42:40 +0800

    proper tagging

---
+++ scripts/build_docker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+CURR_FILE_DIR=`dirname $0`
+PROJECT_DIR=${CURR_FILE_DIR}/../
+PROJECT_NAME=vue_koa_ts_seed_server
+COMMIT_SHA=`git rev-parse --verify HEAD`
+COMMIT_SHA_SHORT=${COMMIT_SHA:0:8}
+DOCKER_IMAGE_LATEST=${PROJECT_NAME}:latest
+DOCKER_IMAGE_WITH_VERSION=${PROJECT_NAME}:${COMMIT_SHA_SHORT}
+
+cd ${PROJECT_DIR}
+
+# build and tag latest
+docker build -t ${DOCKER_IMAGE_WITH_VERSION} -f docker/DockerFile .
+echo ${DOCKER_IMAGE_WITH_VERSION}
+echo ${DOCKER_IMAGE_LATEST}
+docker tag ${DOCKER_IMAGE_WITH_VERSION} ${DOCKER_IMAGE_LATEST}
